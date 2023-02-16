4dce044ba963e7ac22ec92aec0045b832afbe8dd
peppermint100 <krpeppermint100@gmail.com>
2023-02-16 19:39:31 +0900

    init

---
+++ UploadImageWithCoreData/UploadImageWithCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  UploadImageWithCoreDataApp.swift
+//  UploadImageWithCoreData
+//
+//  Created by peppermint100 on 2023/02/16.
+//
+
+import SwiftUI
+
+@main
+struct UploadImageWithCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
